98aef9398104e01a8b9e0d3de577f457748e15be
Victor <vesbru@yahoo.fr>
2024-12-19 22:04:56 +0100

    removed all files from data_story branch

--- src/utils/general_utils.py
+++
@@ -1,4 +0,0 @@
-def number_to_emoji(number: int):
-    # Utilitary function to transform any number into a sequence of digit emojis
-    # Used for artistic purposes only
-    return ''.join([f'{chr(ord("0") + int(digit))}\uFE0F\u20E3' for digit in str(number)])
